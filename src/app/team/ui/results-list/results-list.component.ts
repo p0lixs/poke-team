571909dcef4d014ac2e69a867984e3701b870314
D816436 <D816436@EST092106>
2025-10-01 11:44:33 +0200

    mejoras visuales

--- src/app/team/ui/results-list/results-list.component.ts
+++ src/app/team/ui/results-list/results-list.component.ts
@@ -1,18 +1,19 @@
-import { NgFor } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { PokemonVM } from '../../models/view.model';
 import { TypeIcon } from '../../../shared/ui/type-icon/type-icon';
+import { PokemonVM } from '../../models/view.model';
 
 @Component({
   standalone: true,
   selector: 'app-results-list',
-  imports: [ TypeIcon],
+  imports: [TypeIcon],
   styleUrls: ['./results-list.component.scss'],
-  templateUrl:'./results-list.component.html',
+  templateUrl: './results-list.component.html',
 })
 export class ResultsListComponent {
   @Input() results: PokemonVM[] = [];
   @Output() add = new EventEmitter<PokemonVM>();
 
-  trackById(_i: number, p: PokemonVM) { return p.id; }
+  trackById(_i: number, p: PokemonVM) {
+    return p.id;
+  }
 }
